763d103f128e0957f4080cd78ee191711d2490b9
Ajit Dhivar <ajitdhivar4d@gmail.com>
2024-08-15 13:20:01 +0530

    update

--- dist/app.js
+++ dist/app.js
@@ -1,6 +1,8 @@
+import { config } from "dotenv";
 import express from "express";
 const app = express();
-const port = 3000;
+config({ path: "./.env" });
+const port = process.env.PORT || 3000;
 app.get("/new", (req, res) => {
     res.send("Hello, World!");
 });
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
+import { config } from "dotenv";
 import express, { Request, Response } from "express";
 
 const app = express();
 
-const port = 3000;
+config({ path: "./.env" });
+
+const port = process.env.PORT || 3000;
 
 app.get("/new", (req: Request, res: Response) => {
   res.send("Hello, World!");
